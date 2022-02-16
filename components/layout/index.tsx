0de745db7d6077a44aa65db12cf6fd3462dc50ab
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-15 19:37:03 -0600

    refactor: move TrackPlayer to page from layout

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,14 +1,12 @@
 import Header from '@components/layout/Header'
 import AppContainer from '@components/layout/AppContainer'
-import TrackPlayer from '@components/TrackPlayer'
 
 const Layout = ({ children }) => {
   return (
     <AppContainer>
-      <main className="max-w-6xl min-h-screen max-h-screen m-auto flex flex-col items-center justify-between relative">
+      <main className="max-w-6xl min-h-screen max-h-screen m-auto flex flex-col items-center justify-start relative">
         <Header />
         {children}
-        <TrackPlayer />
       </main>
     </AppContainer>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,54 +1,13 @@
 import Layout from '@components/layout'
 import TrackList from '@components/trackList'
+import TrackPlayer from '@components/trackPlayer'
+import { tracks } from '@lib/data'
 
-const tracks = [
-  {
-    id: '1',
-    title: 'Track 1',
-    artist: 'Artist 1',
-    artwork: 'https://picsum.photos/200/200',
-  },
-  {
-    id: '2',
-    title: 'Track 2',
-    artist: 'Artist 2',
-    artwork: 'https://picsum.photos/200/200',
-  },
-  {
-    id: '3',
-    title: 'Track 3',
-    artist: 'Artist 3',
-    artwork: 'https://picsum.photos/200/200',
-  },
-  {
-    id: '4',
-    title: 'Track 2',
-    artist: 'Artist 2',
-    artwork: 'https://picsum.photos/200/200',
-  },
-  {
-    id: '5',
-    title: 'Track 3',
-    artist: 'Artist 3',
-    artwork: 'https://picsum.photos/200/200',
-  },
-  {
-    id: '6',
-    title: 'Track 2',
-    artist: 'Artist 2',
-    artwork: 'https://picsum.photos/200/200',
-  },
-  {
-    id: '7',
-    title: 'Track 3',
-    artist: 'Artist 3',
-    artwork: 'https://picsum.photos/200/200',
-  },
-]
 export default function App() {
   return (
     <Layout>
       <TrackList tracks={tracks} />
+      <TrackPlayer />
     </Layout>
   )
 }
