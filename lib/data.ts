80f8268ecdce54407b76e091599da65b2be58e8c
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-15 19:36:14 -0600

    refactor: move data to own file

---
+++ lib/data.ts
@@ -0,0 +1,46 @@
+const tracks = [
+  {
+    id: '1',
+    title: 'Track 1',
+    artist: 'Artist 1',
+    artwork: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '2',
+    title: 'Track 2',
+    artist: 'Artist 2',
+    artwork: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '3',
+    title: 'Track 3',
+    artist: 'Artist 3',
+    artwork: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '4',
+    title: 'Track 2',
+    artist: 'Artist 2',
+    artwork: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '5',
+    title: 'Track 3',
+    artist: 'Artist 3',
+    artwork: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '6',
+    title: 'Track 2',
+    artist: 'Artist 2',
+    artwork: 'https://picsum.photos/200/200',
+  },
+  {
+    id: '7',
+    title: 'Track 3',
+    artist: 'Artist 3',
+    artwork: 'https://picsum.photos/200/200',
+  },
+]
+
+export { tracks }
