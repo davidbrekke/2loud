fd6202d97fac11a162a25fb21a1e8f6a8096420a
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-19 09:57:17 -0600

    implement auth

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router'
+
 import Logo from '@components/layout/Logo'
 import { useAuth } from '@lib/hooks/useAuth'
 
@@ -8,20 +9,27 @@ const Header = () => {
   return (
     <header className="flex flex-row items-center w-screen max-w-6xl justify-around py-4 absolute z-10 top-0 backdrop-filter backdrop-blur-lg bg-opacity-30">
       <Logo />
-      {session && (
-        <div className="flex flex-row items-center space-x-2">
+      {session ? (
+        <div className="flex flex-row items-center space-x-4">
           <div
-            className="px-2 py-1 text-lg rounded text-white bg-white bg-opacity-20 shadow transition hover:shadow-xl cursor-pointer hover:bg-opacity-30"
+            className="px-2 py-1 text-lg rounded-lg text-white bg-white bg-opacity-10 shadow-lg transition hover:shadow-xl cursor-pointer hover:bg-opacity-20 hover:scale-105"
             onClick={() => router.push('profile')}
           >
             profile
           </div>
           <button
-            className="px-2 py-1 text-sm rounded text-white cursor-pointer transition hover:bg-opacity-20 hover:bg-white hovre:shadow-xl"
+            className="px-2 py-1 text-sm rounded-md text-white transition hover:shadow-xl cursor-pointer hover:bg-white hover:bg-opacity-10 hover:scale-105"
             onClick={signOut}
           >
             sign out
           </button>
+        </div>
+      ) : (
+        <div
+          className="px-2 py-1 text-lg rounded-lg text-white bg-white bg-opacity-10 shadow-lg transition hover:shadow-xl cursor-pointer hover:bg-opacity-20 hover:scale-105"
+          onClick={() => router.push('/signin')}
+        >
+          sign in
         </div>
       )}
     </header>
