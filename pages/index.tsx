8262d9a3dd03c5d720d00c530979703f6309ecf4
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-19 09:56:17 -0600

    update landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import { useAuth } from '@lib/hooks/useAuth'
 import Layout from '@components/layout'
 import TrackList from '@components/trackList'
 import TrackPlayer from '@components/trackPlayer'
-import { Auth } from '@components/auth'
 import { tracks } from '@lib/data'
 
 export default function App() {
-  const { session } = useAuth()
   return (
     <Layout>
-      {!session ? <Auth /> : <TrackList tracks={tracks} />}
+      <TrackList tracks={tracks} />
       <TrackPlayer />
     </Layout>
   )
