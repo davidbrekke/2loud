4e896be53c5d99b9ca76a8d25d6e5a5429af5645
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-18 18:23:00 -0600

    update state based on session status

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -1,9 +1,31 @@
+import { useRouter } from 'next/router'
 import Logo from '@components/layout/Logo'
+import { useAuth } from '@lib/hooks/useAuth'
 
-const Header = () => (
-  <header className="flex flex-row items-center w-screen max-w-6xl justify-around py-4 absolute z-10 top-0 backdrop-filter backdrop-blur-lg bg-opacity-30">
-    <Logo />
-  </header>
-)
+const Header = () => {
+  const { session, signOut } = useAuth()
+  const router = useRouter()
+  return (
+    <header className="flex flex-row items-center w-screen max-w-6xl justify-around py-4 absolute z-10 top-0 backdrop-filter backdrop-blur-lg bg-opacity-30">
+      <Logo />
+      {session && (
+        <div className="flex flex-row items-center space-x-2">
+          <div
+            className="px-2 py-1 text-lg rounded text-white bg-white bg-opacity-20 shadow transition hover:shadow-xl cursor-pointer hover:bg-opacity-30"
+            onClick={() => router.push('profile')}
+          >
+            profile
+          </div>
+          <button
+            className="px-2 py-1 text-sm rounded text-white cursor-pointer transition hover:bg-opacity-20 hover:bg-white hovre:shadow-xl"
+            onClick={signOut}
+          >
+            sign out
+          </button>
+        </div>
+      )}
+    </header>
+  )
+}
 
 export default Header
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,15 @@
+import { useAuth } from '@lib/hooks/useAuth'
 import Layout from '@components/layout'
 import TrackList from '@components/trackList'
 import TrackPlayer from '@components/trackPlayer'
+import { Auth } from '@components/login'
 import { tracks } from '@lib/data'
 
 export default function App() {
+  const { session } = useAuth()
   return (
     <Layout>
-      <TrackList tracks={tracks} />
+      {!session ? <Auth /> : <TrackList tracks={tracks} />}
       <TrackPlayer />
     </Layout>
   )
