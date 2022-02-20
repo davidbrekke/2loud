c3558b8aca73be35b278c24a6146773ef246ebe9
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-20 13:34:37 -0600

    update add track page styling

--- pages/addtrack/index.tsx
+++ pages/addtrack/index.tsx
@@ -19,57 +19,85 @@ const AddTrack = () => {
     handleAddTrack,
   } = useAddTrack()
 
+  const isFormComplete = title && audioUrl && artworkUrl
+
   return (
     <Layout>
-      <div className="h-screen flex flex-col justify-center items-center">
+      <div className="h-screen flex flex-col justify-center items-center text-gray-700">
         {addingTrack ? (
           <h1>adding track</h1>
         ) : (
           <>
-            <h1>Add Track</h1>
-            <div>
-              <div>
-                <label htmlFor="title">title</label>
+            <h1 className="text-3xl font-bold mb-4">add track</h1>
+            <div className="flex flex-col items-center space-y-6">
+              {/* TITLE */}
+              <div className="flex flex-col">
+                <label htmlFor="title" className="text-lg text-gray-500 ml-2">
+                  title
+                </label>
                 <input
                   type="text"
                   value={title}
                   name="title"
+                  className="rounded-md text-xl outline-none bg-white bg-opacity-30 py-2 px-4 shadow-lg transition focus:shadow-2xl"
+                  placeholder="title..."
                   onChange={(e) => setTitle(e.target.value)}
                 />
               </div>
-              <div>
-                <label htmlFor="title">artwork</label>
-                <input
-                  type="file"
-                  name="artwork"
-                  accept="image/*"
-                  onChange={handleArtworkChange}
-                />
-                {uploadingArtwork && <span>uploading</span>}
-                {artworkPreview && (
-                  <Image width={150} height={150} src={artworkPreview} />
-                )}
+              {/* FILES */}
+              <div className="flex flex-col md:flex-row">
+                {/* ARTWORK */}
+                <div className="flex flex-col items-center p-2">
+                  <label htmlFor="title" className="text-lg text-gray-500 ml-2">
+                    artwork
+                  </label>
+                  <input
+                    type="file"
+                    name="artwork"
+                    accept="image/*"
+                    onChange={handleArtworkChange}
+                    className="text-gray-600 p-2"
+                  />
+                  {artworkPreview ? (
+                    <Image width={200} height={200} src={artworkPreview} />
+                  ) : (
+                    <div className="overflow-hidden rounded-xl w-48 h-48 bg-white bg-opacity-30 text-gray-400 flex items-center justify-center shadow-lg">
+                      preview
+                    </div>
+                  )}
+                  {uploadingArtwork && <span>uploading</span>}
+                </div>
+                {/* AUDIO */}
+                <div className="flex flex-col items-center p-2">
+                  <label htmlFor="title" className="text-lg text-gray-500 ml-2">
+                    audio
+                  </label>
+                  <input
+                    type="file"
+                    name="audio"
+                    accept="audio/*"
+                    onChange={handleAudioChange}
+                    className="text-gray-600 p-2"
+                  />
+                  {audioPreview && <audio controls src={audioPreview} />}
+                  {uploadingAudio && <span>uploading</span>}
+                </div>
               </div>
-              <div>
-                <label htmlFor="title">audio</label>
-                <input
-                  type="file"
-                  name="audio"
-                  accept="audio/*"
-                  onChange={handleAudioChange}
-                />
-                {uploadingAudio && <span>uploading</span>}
-                {audioPreview && <audio controls src={audioPreview} />}
-              </div>
+
+              {/* SUBMIT */}
+              {isFormComplete ? (
+                <div
+                  onClick={handleAddTrack}
+                  className="px-4 py-2 rounded-lg text-gray-100 shadow-lg transition text-lg hover:scale-105 hover:shadow-xl cursor-pointer bg-gradient-to-br from-teal-500 via-indigo-400 to-indigo-500"
+                >
+                  add track
+                </div>
+              ) : (
+                <div className="px-4 py-2 rounded-lg text-gray-100 shadow-lg text-lg bg-white bg-opacity-30">
+                  add track
+                </div>
+              )}
             </div>
-            {title && audioUrl && artworkUrl && (
-              <div
-                onClick={handleAddTrack}
-                className="px-4 py-2 rounded-lg text-gray-100 shadow-lg transition text-lg hover:scale-105 hover:shadow-xl cursor-pointer bg-gradient-to-br from-teal-500 via-indigo-400 to-indigo-500"
-              >
-                add track
-              </div>
-            )}
           </>
         )}
       </div>
