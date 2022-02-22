926f9656121af4cbe36a049928c7f88bb6ea4929
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-21 19:56:23 -0600

    add head element

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,14 +1,22 @@
+import Head from 'next/head'
 import Header from '@components/layout/header'
 import AppContainer from '@components/layout/AppContainer'
 
 const Layout = ({ children }) => {
   return (
-    <AppContainer>
-      <main className="max-w-6xl min-h-screen max-h-screen m-auto flex flex-col items-center justify-start relative">
-        <Header />
-        {children}
-      </main>
-    </AppContainer>
+    <>
+      <Head>
+        <title>2loud</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="2loud" />
+      </Head>
+      <AppContainer>
+        <main className="max-w-6xl min-h-screen max-h-screen m-auto flex flex-col items-center justify-start relative">
+          <Header />
+          {children}
+        </main>
+      </AppContainer>
+    </>
   )
 }
 
