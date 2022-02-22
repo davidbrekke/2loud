5963b6d96a63c9dcc47a5b9ec981c8a5acf5fede
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-22 11:21:04 -0600

    update env vars

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,12 +1,12 @@
 import { createClient } from '@supabase/supabase-js'
 import {
   NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_KEY,
+  NEXT_PUBLIC_SUPABASE_ANON_KEY,
 } from '@lib/constants'
 
 const supabase = createClient(
   NEXT_PUBLIC_SUPABASE_URL,
-  NEXT_PUBLIC_SUPABASE_KEY
+  NEXT_PUBLIC_SUPABASE_ANON_KEY
 )
 
 export { supabase }
