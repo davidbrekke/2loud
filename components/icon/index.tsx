229f330fd9c093d0a1fb339f2d81e12e35eb9069
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-21 18:46:03 -0600

    update icon

--- components/icon/index.tsx
+++ components/icon/index.tsx
@@ -2,10 +2,12 @@ const Icon = ({
   icon,
   onClick,
   size,
+  color,
 }: {
   icon: any
   onClick: any | null
   size: 'sm' | 'md' | 'lg'
+  color: 'orange' | 'white'
 }) => (
   <div
     onClick={onClick ? onClick : null}
@@ -13,7 +15,9 @@ const Icon = ({
         ${size === 'sm' && 'w-5 h-5'}
         ${size === 'md' && 'w-7 h-7'}
         ${size === 'lg' && 'w-10 h-10'}
-        text-white cursor-pointer transition drop-shadow hover:drop-shadow-xl hover:scale-110`}
+        ${color === 'orange' && 'text-orange-200'}
+        ${color === 'white' && 'text-white'}
+         cursor-pointer transition drop-shadow hover:drop-shadow-xl hover:scale-110`}
   >
     {icon}
   </div>
