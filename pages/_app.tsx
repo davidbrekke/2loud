de606ce36c90ad533cccba3af152a1a523fe4195
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-22 11:21:46 -0600

    configure supabase auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,12 @@
+import type { AppProps } from 'next/app'
+import { UserProvider } from '@supabase/supabase-auth-helpers/react'
+import { supabaseClient } from '@supabase/supabase-auth-helpers/nextjs'
 import 'tailwindcss/tailwind.css'
 
-export default function _App({ Component, ...pageProps }) {
-  return <Component {...pageProps} />
+export default function _App({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider supabaseClient={supabaseClient}>
+      <Component {...pageProps} />
+    </UserProvider>
+  )
 }
