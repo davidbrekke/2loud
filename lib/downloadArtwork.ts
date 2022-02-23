3415318e6734bb195c82e0bd7ba9141c6f58ea13
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-22 18:57:51 -0600

    download artwork as url function

---
+++ lib/downloadArtwork.ts
@@ -0,0 +1,17 @@
+import { supabase } from '@lib/supabase'
+
+const downloadArtworkAsUrl = async (path: string) => {
+  try {
+    const { data, error } = await supabase.storage
+      .from('artwork')
+      .download(path)
+    if (error) {
+      throw error
+    }
+    return URL.createObjectURL(data)
+  } catch (error) {
+    console.log('Error downloading artwork: ', error.message)
+  }
+}
+
+export { downloadArtworkAsUrl }
