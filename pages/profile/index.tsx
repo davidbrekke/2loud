417bfeb321e827fda7a30b62aab22ac511a41f68
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-02-28 17:06:07 -0600

    add uploading avatar functionality

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,17 +1,52 @@
 import { withAuthRequired } from '@supabase/supabase-auth-helpers/nextjs'
+import { useEffect, useState } from 'react'
 
 import Layout from '@components/layout'
 import UserTracks from '@components/profile/UserTracks'
 import UserDetails from '@components/profile/UserDetails'
+import { useAuth } from '@lib/hooks/useAuth'
+import { supabase } from '@lib/supabase'
+import { checkUserRegistration } from '@lib/checkUserRegistration'
 
 const Profile = () => {
+  const [profile, setProfile] = useState(null)
+  const { user } = useAuth()
+
+  useEffect(() => {
+    ;(async () => {
+      if (profile) return
+      const { registered, userProfile } = await checkUserRegistration(user)
+      if (!registered) {
+        const { data: insertData, error: insertError } = await supabase
+          .from('profiles')
+          .insert([
+            {
+              id: user?.id,
+              email: user?.email,
+            },
+          ])
+        if (insertError) {
+          console.error(insertError)
+        }
+        setProfile(insertData[0])
+      }
+      if (registered && userProfile) {
+        console.log('registered', registered)
+        console.log('userProfile', userProfile)
+        setProfile(userProfile)
+      }
+    })()
+  }, [])
+
   return (
-    <Layout>
-      <div className="w-screen flex flex-col items-center pt-32">
-        <UserDetails />
-        <UserTracks />
-      </div>
-    </Layout>
+    profile && (
+      <Layout>
+        <div className="w-screen flex flex-col items-center pt-32">
+          <UserDetails profile={profile} />
+          <UserTracks />
+        </div>
+      </Layout>
+    )
   )
 }
 
