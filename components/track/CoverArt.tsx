39bda5c4cee30dbb6acf50910fd2f28ada9b710c
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-01 17:20:26 -0600

    refactor cover art component

--- components/track/CoverArt.tsx
+++ components/track/CoverArt.tsx
@@ -1,8 +1,9 @@
-import { useContext } from 'react'
 import Image from 'next/image'
-import { useState, useEffect } from 'react'
-import { supabase } from '@lib/supabase'
+import { useState, useEffect, useContext } from 'react'
+import { MusicNoteIcon } from '@heroicons/react/solid'
+
 import { TrackContext } from '@lib/contexts/TrackContext'
+import { downloadArtworkAsUrl } from '@lib/downloadArtwork'
 
 const CoverArt = () => {
   const [coverArtUrl, setCoverArtUrl] = useState<string | null>(null)
@@ -10,36 +11,27 @@ const CoverArt = () => {
   const { artwork_url } = useContext(TrackContext)
 
   useEffect(() => {
-    if (artwork_url) downloadArtwork(artwork_url)
+    ;(async () => {
+      if (artwork_url) {
+        const url = await downloadArtworkAsUrl(artwork_url)
+        setCoverArtUrl(url)
+      }
+    })()
   }, [artwork_url])
 
-  const downloadArtwork = async (path: string) => {
-    try {
-      const { data, error } = await supabase.storage
-        .from('artwork')
-        .download(path)
-      if (error) {
-        throw error
-      }
-      const url = URL.createObjectURL(data)
-      setCoverArtUrl(url)
-    } catch (error) {
-      console.log('Error downloading image: ', error.message)
-    }
-  }
   return coverArtUrl ? (
-    <div className="w-24 md:w-32">
+    <div className="w-24">
       <Image
         src={coverArtUrl}
-        width={200}
-        height={200}
+        width={100}
+        height={100}
         className="rounded-2xl"
         alt="cover art"
       />
     </div>
   ) : (
-    <div className="w-24 md:w-32">
-      <span>artwork</span>
+    <div className="w-24 flex flex-row items-center justify-center">
+      <MusicNoteIcon className="text-white w-16 drop-shadow-xl" />
     </div>
   )
 }
