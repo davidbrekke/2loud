a0ccd67ab3af34fc176b783cf654b3788c575266
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-01 15:24:21 -0600

    function to checkif a username exists

---
+++ lib/checkUsername.ts
@@ -0,0 +1,20 @@
+import { supabase } from '@lib/supabase'
+
+const checkUsername = async (username: string) => {
+  try {
+    let { data: usernameData, error: usernameError } = await supabase
+      .from('profiles')
+      .select('*')
+      .eq('username', username)
+
+    if (usernameError) {
+      throw usernameError
+    }
+    // return true if username is found, false if not
+    return usernameData.length ? true : false
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+export { checkUsername }
