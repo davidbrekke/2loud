fdfc57344dc3f5bd36129f4e8f195a76866af39f
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-04 14:25:01 -0600

    add error handling, remove logs

--- lib/downloadTrack.ts
+++ lib/downloadTrack.ts
@@ -1,19 +1,21 @@
 import { supabase } from '@lib/supabase'
 const downloadTrack = async (audioUrl: string) => {
-  if (confirm('Download track?')) {
-    console.log('Download track', audioUrl)
-    const { data, error } = await supabase.storage
-      .from('audio')
-      .download(audioUrl)
-    if (error) {
-      console.error(error)
+  try {
+    if (confirm('Download track?')) {
+      const { data, error } = await supabase.storage
+        .from('audio')
+        .download(audioUrl)
+      if (error) {
+        console.error(error)
+      }
+      const anchor = document.createElement('a')
+      const localUrl = URL.createObjectURL(data)
+      anchor.href = localUrl
+      anchor.download = audioUrl
+      anchor.click()
     }
-    console.log('Downloaded track', data)
-    const anchor = document.createElement('a')
-    const localUrl = URL.createObjectURL(data)
-    anchor.href = localUrl
-    anchor.download = audioUrl
-    anchor.click()
+  } catch (e) {
+    console.error(e)
   }
 }
 
