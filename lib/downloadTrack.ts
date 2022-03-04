bb9b061eeab882747d9ed1aee535b90ce253a4fe
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-04 14:21:28 -0600

    add download track funcitonality

--- lib/downloadTrack.ts
+++ lib/downloadTrack.ts
@@ -1,5 +1,20 @@
-const downloadTrack = async () => {
-  confirm('Download track?')
+import { supabase } from '@lib/supabase'
+const downloadTrack = async (audioUrl: string) => {
+  if (confirm('Download track?')) {
+    console.log('Download track', audioUrl)
+    const { data, error } = await supabase.storage
+      .from('audio')
+      .download(audioUrl)
+    if (error) {
+      console.error(error)
+    }
+    console.log('Downloaded track', data)
+    const anchor = document.createElement('a')
+    const localUrl = URL.createObjectURL(data)
+    anchor.href = localUrl
+    anchor.download = audioUrl
+    anchor.click()
+  }
 }
 
 export { downloadTrack }
