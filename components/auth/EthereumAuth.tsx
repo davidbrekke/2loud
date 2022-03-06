d43d683a1dc2658563e6f56999a71de18ab7767d
davidbrekke <davidmagnuson@icloud.com>
2022-03-06 13:21:30 -0600

    add details element and begin eth auth

---
+++ components/auth/EthereumAuth.tsx
@@ -0,0 +1,10 @@
+const EthereumAuth = () => {
+  return (
+    <div className="flex flex-col items center gap-4">
+      <h1>login with ethereum</h1>
+      <div>connect wallet</div>
+    </div>
+  )
+}
+
+export default EthereumAuth
--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -1,9 +1,19 @@
-import { Auth } from '@components/auth'
+import EmailAuth from '@components/auth/EmailAuth'
+import EthereumAuth from '@components/auth/EthereumAuth'
 import Layout from '@components/layout'
 
 const SignIn = () => (
   <Layout>
-    <Auth />
+    <div className="h-screen flex flex-col justify-center">
+      <details>
+        <summary>sign in with email</summary>
+        <EmailAuth />
+      </details>
+      <details>
+        <summary>sign in with ethereum</summary>
+        <EthereumAuth />
+      </details>
+    </div>
   </Layout>
 )
 
