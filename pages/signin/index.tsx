5579a08ee7139ca3398eeaae05302857c3eca1c3
David Magnuson <52757864+davidbrekke@users.noreply.github.com>
2022-03-08 10:30:53 -0600

    add signin header

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -5,6 +5,7 @@ import Layout from '@components/layout'
 const SignIn = () => (
   <Layout>
     <div className="h-screen flex flex-col items-center justify-center gap-6 w-80 md:w-auto">
+      <h2 className="text-2xl text-gray-700">choose sign in method</h2>
       <details className="bg-white bg-opacity-10 p-4 rounded-xl shadow-lg">
         <summary className="cursor-pointer text-lg text-gray-600 text-center outline-none">
           sign in with email
